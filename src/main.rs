f6993ecf5e8af9a60b58db6e9e296c7ca1438f26
AndyDaMandy <maldonado.andy.m@gmail.com>
2024-01-05 12:53:41 -0500

    added item struct and moves

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    game();
 }
 
 fn game() {
@@ -10,8 +10,12 @@ fn game() {
 
     let mut goblin = Enemy::new("Goblin", 100, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
 
-
-
+    let mut round = 0;
+    while main_character.health > 0 || goblin.health > 0 {
+        println!("Round {}", round);
+        battle(main_character, goblin);
+        round += 1;
+    }
 }
 
 struct Character {
@@ -44,11 +48,27 @@ struct Enemy {
     proficiency_bonus: i32
 }
 
-fn battle(player: Character, enemy: Enemy) {
+struct Item {
+    name: String,
+    description: String,
+    value: i32,
+    weight: i32,
+    effect: Effect::Heal,
+    power: i32
+}
+
+enum Effect {
+    Heal,
+    Damage,
+    Buff,
+    Debuff
+}
+
+fn battle(mut player: Character, mut enemy: Enemy, item: Item) {
     let mut turn = 0;
     while player.health > 0 || enemy.health > 0 {
         if turn % 2 == 0 {
-            player_turn(player, enemy);
+            player_turn(player, enemy, item: Item);
         } else {
             enemy_turn(player, enemy);
         }
@@ -56,7 +76,7 @@ fn battle(player: Character, enemy: Enemy) {
     }
 }
 
-fn player_turn(player: Character, enemy: Enemy) {
+fn player_turn(player: Character, enemy: Enemy, item: Item) {
     println!("It's your turn!");
     println!("What would you like to do?");
     println!("1. Attack");
@@ -69,20 +89,42 @@ fn player_turn(player: Character, enemy: Enemy) {
     match input {
         1 => attack(player, enemy),
         2 => defend(player),
-        3 => use_item(player),
-        4 => run(player, enemy),
+        3 => use_item(player, item),
         _ => println!("Please enter a valid number!")
     }
 }
 
+fn enemy_turn(player: Character, enemy: Enemy) {
+    println!("It's the enemy's turn!");
+    let mut rng = rand::thread_rng();
+    let attack_roll = rng.gen_range(1..20);
+    if attack_roll + enemy.strength > player.armor_class {
+        let damage_roll = rng.gen_range(1..8);
+        player.health -= damage_roll + enemy.strength;
+        println!("The enemy hit you for {} damage!", damage_roll + enemy.strength);
+    } else {
+        println!("The enemy missed!");
+    }
+
+}
+
 fn attack(player: Character, mut enemy: Enemy) {
     let mut rng = rand::thread_rng();
-    let attack_roll = rng.gen_range(1, 20);
+    let attack_roll = rng.gen_range(1..20);
     if attack_roll + player.strength > enemy.armor_class {
-        let damage_roll = rng.gen_range(1, 8);
+        let damage_roll = rng.gen_range(1..8);
         enemy.health -= damage_roll + player.strength;
         println!("You hit the enemy for {} damage!", damage_roll + player.strength);
     } else {
         println!("You missed!");
     }
+}
+
+fn defend(mut player: Character) {
+    println!("You defend!");
+    player.armor_class += 2;
+}
+
+fn use_item(player: Character, item: Item) {
+    println!("You use an item!");
 }
